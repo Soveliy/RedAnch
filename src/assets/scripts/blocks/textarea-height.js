2be3dc5d3df1f10e6e61c42a48195c04b603e721
Soveliy <nikita.elagin2012@yandex.ru>
2022-12-16 14:57:18 +0300

    upload

--- src/assets/scripts/blocks/textarea-height.js
+++ src/assets/scripts/blocks/textarea-height.js
@@ -2,38 +2,40 @@
 
 let observe;
 let text = document.querySelector('.add__text');
-
-if (window.attachEvent) {
-    observe = function (element, event, handler) {
-        element.attachEvent('on'+event, handler);
-    };
-} else {
-    observe = function (element, event, handler) {
-        element.addEventListener(event, handler, false);
-    };
-}
-
-function init() {
-    function resize () {
-        text.style.height = 'auto';
-        text.style.height = text.scrollHeight+'px';
+if (text){
+    if (window.attachEvent) {
+        observe = function (element, event, handler) {
+            element.attachEvent('on'+event, handler);
+        };
+    } else {
+        observe = function (element, event, handler) {
+            element.addEventListener(event, handler, false);
+        };
     }
-
-    function delayedResize() {
-        requestAnimationFrame(function() {
-            resize();
-        });
-
+    
+    function init() {
+        function resize () {
+            text.style.height = 'auto';
+            text.style.height = text.scrollHeight+'px';
+        }
+    
+        function delayedResize() {
+            requestAnimationFrame(function() {
+                resize();
+            });
+    
+        }
+        observe(text, 'change',  resize);
+        observe(text, 'cut',     delayedResize);
+        observe(text, 'paste',   delayedResize);
+        observe(text, 'drop',    delayedResize);
+        observe(text, 'keydown', delayedResize);
+    
+        text.focus();
+        text.select();
+        resize();
     }
-    observe(text, 'change',  resize);
-    observe(text, 'cut',     delayedResize);
-    observe(text, 'paste',   delayedResize);
-    observe(text, 'drop',    delayedResize);
-    observe(text, 'keydown', delayedResize);
-
-    text.focus();
-    text.select();
-    resize();
+    
+    init();
+    
 }
-
-init();
